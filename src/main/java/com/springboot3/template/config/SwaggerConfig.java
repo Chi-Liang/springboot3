141d99a26e09790bc63c5d3ce0b1d6d9aa5f3eed
klchib3 <klchib3@gmail.com>
2025-01-11 10:53:43 +0800

    fix api nginx

--- src/main/java/com/springboot3/template/config/SwaggerConfig.java
+++ src/main/java/com/springboot3/template/config/SwaggerConfig.java
@@ -5,8 +5,12 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
+        servers = {
+                @Server(url = "${project.server-url}")
+        },
         security = @io.swagger.v3.oas.annotations.security.SecurityRequirement(
                 name = "Bearer Authentication"
         )
